ca765a9e7f02c0cabe2f5b3f6c95bcd09ec4e96b
justanotheruser <hurrdurrrderp@gmail.com>
2020-04-28 15:01:42 +0300

    Test Django REST framework

--- player/models.py
+++ player/models.py
@@ -4,11 +4,17 @@ from django.db import models
 class Artist(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Album(models.Model):
     title = models.CharField(max_length=200)
     release_date = models.DateField()
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 
 class Track(models.Model):
@@ -16,3 +22,6 @@ class Track(models.Model):
     length = models.PositiveSmallIntegerField()
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
--- spotify/settings.py
+++ spotify/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'player.apps.PlayerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
